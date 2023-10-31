8392bc94b8520ad7605a0de345f9c7f39703be0c
Lakshmansai Katteragandla <klakshmansai@stratapps.com>
2023-10-31 14:22:41 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Notification.Demo',
+  appName: 'andrroid',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
